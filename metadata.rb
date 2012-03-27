9fcc1874fe6a050c40bcb17065213684626a1f28
Josiah Kiehl <jkiehl@riotgames.com>
2012-03-27 15:26:16 -0700

    Install p4 via homebrew

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Josiah Kiehl"
+maintainer_email "josiah@bluepojo.com"
+license          "All rights reserved"
+description      "Installs/Configures perforce"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+depends "homebrew", "~> 1.0.0"
---
+++ recipes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: perforce
+# Recipe:: default
+#
+# Author:: Josiah Kiehl <josiah@bluepojo.com>
+#
+# Copyright 2012, Riot Games
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'homebrew'
+
+package 'perforce'
