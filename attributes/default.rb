360ab07d74dd7befaa0862b950bf9aad961bb7df
Josiah Kiehl <jkiehl@riotgames.com>
2012-03-27 17:37:48 -0700

    p4 configs

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,14 +8,12 @@
 # All rights reserved - Do Not Redistribute
 #
 
-default[:user]             = 'jkiehl' # will be node attributes
+default[:owner]             = 'jkiehl' # will be node attributes
 default[:group]            = 'staff'
 
-default[:p4][:workspace_path] = "/Users/#{default[:user]}/p4workspace/"
+default[:p4][:port]           = 'PerfLAX01:1666' # environment attribute
 
-default[:p4][:port]           = nil # environment attribute
-
-default[:p4][:user]           = default[:user]
+default[:p4][:user]           = default[:owner]
 default[:p4][:passwd]         = nil
 
 default[:p4][:diff]           = nil
@@ -24,7 +22,7 @@ default[:p4][:merge]          = nil
 
 default[:p4][:client]         = nil
 
-default[:p4][:charset]        = nil
+default[:p4][:charset]        = 'utf8'
 default[:p4][:commandcharset] = nil
 default[:p4][:language]       = nil 
 default[:p4][:host]           = nil
--- recipes/default.rb
+++ recipes/default.rb
@@ -13,10 +13,31 @@ include_recipe 'homebrew'
 
 package 'perforce'
 
-directory node[:p4][:workspace_path]
+home_path = File.join("/Users", node[:owner])
+p4config_path = File.join(home_path, '.p4config')
 
-template "#{node[:p4workspace_path]}/.p4config" do
-  owner node[:user]
-  group node[:group]
+template p4config_path do
+  owner     node[:owner]
+  group     node[:group]
   variables node[:p4]
+end
+
+source_p4config_line = "source ~/.p4config"
+
+ruby_block 'source .p4config in .bash_profile' do
+  block do
+    File.open(File.join(home_path, '.bash_profile'), 'a') do |file|
+      file.puts source_p4config_line
+    end
+  end
+
+  not_if do
+    begin
+      File.open(File.join(home_path, '.bash_profile'), 'r') do |file|
+        !!(file.readlines.join(' ') =~ /#{source_p4config_line}/)
+      end
+    rescue Errno::ENOENT
+      false
+    end
+  end
 end
