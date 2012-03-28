aff364de73176a0c1dfe12bcecc5e2850657e2e0
Josiah Kiehl <jkiehl@riotgames.com>
2012-03-27 18:17:47 -0700

    Move attributes to node and env attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,12 +8,12 @@
 # All rights reserved - Do Not Redistribute
 #
 
-default[:owner]             = 'jkiehl' # will be node attributes
-default[:group]            = 'staff'
+default[:p4][:owner]          = nil
+default[:p4][:group]          = nil
 
-default[:p4][:port]           = 'PerfLAX01:1666' # environment attribute
+default[:p4][:port]           = nil # environment attribute
 
-default[:p4][:user]           = default[:owner]
+default[:p4][:user]           = default[:p4][:owner]
 default[:p4][:passwd]         = nil
 
 default[:p4][:diff]           = nil
