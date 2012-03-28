eec0a315766700c28567e8d4cf06e45a19b9e44c
Josiah Kiehl <jkiehl@riotgames.com>
2012-03-28 16:21:06 -0700

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,9 @@ maintainer_email "josiah@bluepojo.com"
 license          "All rights reserved"
 description      "Installs/Configures perforce"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.5"
 
 supports         "mac_os_x"
 
-depends "homebrew", "~> 1.0.0"
+depends          "homebrew", "~> 1.0.0"
 
