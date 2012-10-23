a61a3c973cf86de2128117a418b4ec7e2fb7d43a
Kyle Allan <kallan@riotgames.com>
2012-10-22 18:02:29 -0700

    add template to set environment

--- recipes/linux.rb
+++ recipes/linux.rb
@@ -16,3 +16,12 @@ perforce_p4 "12.1" do
   directory node[:p4][:install_dir]
   sixty_four node[:kernel][:machine] == "x86_64"
 end
+
+template "/etc/profile.d/perforce.sh" do
+  mode 0755
+  source "perforce.sh.erb"
+  variables(:p4_port => node[:p4][:port],
+            :p4_charset => node[:p4][:charset],
+            :p4_user => node[:p4][:user],
+            :p4_config => node[:p4][:config_filename])
+end
